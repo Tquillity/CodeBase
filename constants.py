d46578fca0c11d6d9fc2d584cc0a5a702f1bd62d
Tquillity <mikael.sundh@gmail.com>
2025-09-30 23:18:16 +0200

    Version update

--- constants.py
+++ constants.py
@@ -28,7 +28,7 @@ TEXT_EXTENSIONS_DEFAULT = {
 FILE_SEPARATOR = "===FILE_SEPARATOR===\n"
 
 # Application version
-VERSION = "4.0"
+VERSION = "5.0"
 
 # Cache configuration
 CACHE_MAX_SIZE = 1000  # Maximum number of cached files
